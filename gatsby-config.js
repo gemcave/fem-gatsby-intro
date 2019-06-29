c9adca87d7d0942750696962480dfcea9712795b
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-06-29 09:31:20 +0600

    version for deploy was finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,14 @@ module.exports = {
         username: 'tb_choi',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
