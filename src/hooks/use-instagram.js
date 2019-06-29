82e24f4bcf54ff9ec0aa853c91b17dd3ab22b310
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-06-29 08:57:41 +0600

    insta component and insta hook was added

---
+++ src/hooks/use-instagram.js
@@ -0,0 +1,29 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const useIntagram = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allInstaNode(limit: 12) {
+        nodes {
+          id
+          caption
+          username
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 120, maxHeight: 120) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  return data.allInstaNode.nodes.map(node => ({
+    ...node.localFile.childImageSharp,
+    id: node.id,
+    username: node.username,
+    caption: node.caption,
+  }));
+};
+export default useIntagram;
